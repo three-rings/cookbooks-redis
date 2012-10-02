458f0e8513ca263c0a7eefeef178070683434148
kenjiskywalker <git@kenjiskywalker.org>
2012-10-02 14:27:51 +0900

    config add

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,4 +1,5 @@
 #
+#
 # Cookbook Name:: Redis
 # Recipe:: default
 #
@@ -10,26 +11,27 @@ package "tcl"
 
 bin = "/usr/local/bin/redis-server"
 version  = node[:redis][:version]
-src_file = "http://redis.googlecode.com/files/redis-#{version}.tar.gz"
+file = "redis-#{version}"
+url = "https://redis.googlecode.com/files/redis-#{version}.tar.gz"
 installed_version = `#{bin} -v 2>&1`.chomp.split(/\s/)[3]
 log "installed version: #{installed_version}"
 do_install = ( version != installed_version )
 
 
-cookbook_file "/tmp/#{src_file}" do
-  source src_file 
+remote_file "/tmp/redis-#{version}.tar.gz" do
+  source "#{url}"
+  mode "0644"
   only_if { do_install }
 end
 
 bash "install_redis" do
   cwd "/tmp"
   code <<-EOH
-  tar zxvf #{src_file} && cd redis-#{version} && make && make install && cp utils/redis_init_script /etc/init.d/redis
+    tar zxvf #{file}.tar.gz && cd #{file} && make && make install && cp utils/redis_init_script /etc/init.d/redis && mkdir /etc/redis/ && cp redis.conf /etc/redis/6379.conf
   EOH
   only_if { do_install }
 end
 
-file "/tmp/#{src_file}" do
+file "/tmp/#{file}.tar.gz" do
   action :delete
 end
-
